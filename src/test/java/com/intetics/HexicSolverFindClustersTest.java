377bbdad57785907dd83f906821826dcb99eacb1
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2012-10-31 03:28:33 +0300

    * Added more tests

--- src/test/java/com/intetics/HexicSolverFindClustersTest.java
+++ src/test/java/com/intetics/HexicSolverFindClustersTest.java
@@ -41,4 +41,32 @@ public class HexicSolverFindClustersTest {
         assertTrue(clusters.get(0).contains(10));
         assertTrue(clusters.get(0).contains(5));
     }
+
+    @Test
+    public void testBigGroupedCluster() {
+        solver.getCells()[0][6] = 1;
+        solver.getCells()[6][6] = 1;
+        solver.getCells()[1][6] = 1;
+        solver.getCells()[11][6] = 1;
+        solver.getCells()[21][6] = 1;
+        solver.getCells()[26][6] = 1;
+        solver.getCells()[20][6] = 1;
+        solver.getCells()[25][6] = 1;
+        solver.getCells()[30][6] = 1;
+        solver.getCells()[40][6] = 1;
+        solver.getCells()[50][6] = 1;
+        solver.getCells()[56][6] = 1;
+        solver.getCells()[61][6] = 1;
+        solver.getCells()[67][6] = 1;
+        solver.getCells()[57][6] = 1;
+        solver.getCells()[51][6] = 1;
+        solver.getCells()[47][6] = 1;
+        solver.getCells()[52][6] = 1;
+        solver.getCells()[58][6] = 1;
+
+        System.out.println(solver.getGameBoard());
+        List<Set<Integer>> clusters = solver.findClusters(solver.getCells());
+        assertEquals(1, clusters.size());
+        assertEquals(19, clusters.get(0).size());
+    }
 }
